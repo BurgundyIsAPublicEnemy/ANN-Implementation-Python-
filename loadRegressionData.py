00069f147df30fc05dfe84330553172a1f480637
BurgundyIsAPublicEnemy <veryseriousemailaddress@gmail.com>
2019-11-06 00:45:14 +0000

    Added percentage error

--- loadRegressionData.py
+++ loadRegressionData.py
@@ -6,6 +6,7 @@ logging.basicConfig(level=logging.DEBUG)
 import matplotlib.pyplot as plt
 from pandas import DataFrame
 from sklearn.model_selection import KFold
+from sklearn import metrics
 
 import pandas as pd
 
@@ -34,8 +35,8 @@ n_output = 1
 n_slices = 10
 #Learning parameters
 learning_constant = 0.02
-number_epochs = 1
-batch_size = 10000
+number_epochs = 10
+batch_size = 20000
 
 #Defining the input and the output
 X = tf.placeholder("float", [None, n_input])
@@ -110,7 +111,7 @@ batch_x = np.array_split(batch_x, n_slices)
 batch_y = np.array_split(batch_y, n_slices)
 angle_array = np.array_split(batch_y, n_slices)
 mean_acc = 0
-mean_loss_acc = 0
+mean_per_acc = 0
 for train_index, test_index in kf.split(batch_x):
     print(train_index, test_index)
     label=angle_array[int(test_index)]#+1e-50-1e-50
@@ -154,7 +155,6 @@ for train_index, test_index in kf.split(batch_x):
         print("Accuracy:", ac1)
         print("ACC1:", loss_op.eval({X: batch_x_test, Y: batch_y_test}))
         print("ACC2:", accuracy.eval({X: batch_x_test, Y: batch_y_test}))
-        loss_acc = loss_op.eval({X: batch_x_test, Y: batch_y_test})
         #tf.keras.evaluate(pred,batch_x)
 
         print("Prediction:", pred.eval({X: batch_x_test}))
@@ -178,9 +178,14 @@ for train_index, test_index in kf.split(batch_x):
 
         print (df)
 
-        #print(accuracy)
+        correct_prediction = tf.equal(tf.argmax(pred[0:batch_size], 1),batch_y_test[0:batch_size])
+        # Calculate accuracy
+        accuracy1 = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+        per_acc = accuracy1.eval({X: batch_x_test[0:batch_size], Y: batch_y_test[0:batch_size]})
+        print("Accuracy:", per_acc)
+
         mean_acc += ac1
-        mean_loss_acc += loss_acc
+        mean_per_acc += per_acc
         batch_x_train = []
         batch_y_train = []
 
@@ -189,11 +194,11 @@ mean_acc = mean_acc / n_slices
 try:
     print('Mean accuracy: ', mean_acc)
     f = open("reportlog.txt", "a")
-    f.write('\n\n\n\n')
+    f.write('\n\n\n')
     f.write(("HYPERPARAMS: " + " \n HIDDEN LAYER 1 NO. NODES: " + str(n_hidden1) + "\n HIDDEN LAYER 2 NO. NODES: " + str(n_hidden2) + "\n HIDDEN LAYER 3 NO. NODES: " + str(n_hidden3) + "\n INPUT NODES: " +  str(n_input) + "\n OUTPUT NODES: " + str(n_output) +  "\n NSLICES: " + str(n_slices)))
-    f.write(("HYPERPARAMS 2: \n LEARNING_CONSTANT: " + str(learning_constant) + "\n NO EPOCHS: " + str(number_epochs) + "\n BATCHSIZE: " + str(batch_size)))
-    f.write(("\n MEAN MSE ACC: " + str(mean_acc) + "\n MEAN LOSS ACC: " + str(mean_loss_acc)))
-    f.write('\n\n\n\n')
+    f.write(("\n HYPERPARAMS 2: \n LEARNING_CONSTANT: " + str(learning_constant) + "\n NO EPOCHS: " + str(number_epochs) + "\n BATCHSIZE: " + str(batch_size)))
+    f.write(("\n MEAN MSE ACC: " + str(mean_acc) + "\n MEAN PERCENTAGE ACC: " + str(mean_per_acc)))
+    f.write('\n\n\n')
     f.close()
 except Exception as e:
     print(e)
